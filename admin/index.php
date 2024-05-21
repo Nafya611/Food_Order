6a590a7b950bfef696a3f12c6165290d352609d3
Nafyad <nafyadhailu99@gmail.com>
2024-05-21 19:13:59 +0300

    first comit

---
+++ admin/index.php
@@ -0,0 +1,32 @@
+<html>
+  <head>
+    <title>Food Order website-Home Page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+    
+  <body> 
+    <!-- main section starts-->
+    <div class="menu">
+      <div class="wrapper">
+        Menu goes here
+      </div>
+
+    </div>
+      
+      
+    <!-- main section ends-->
+
+    <!--main content section starts -->
+<div class="main_content">
+  Main content goes here
+  
+</div>
+<!-- Main content section ends  -->
+
+
+<!-- Footer section starts -->
+<div class="footer">
+  Footer goes here
+</div>
+<!-- Footer section ends -->
+
+  </body>
